d8838c4dc85d22dbb03ac1127b1aadf3782846d7
kevinhsu <kevinhsu@synology.com>
2016-03-12 21:54:26 +0800

    tcp_server.py

--- net/tcp_client.py
+++ net/tcp_client.py
@@ -1,14 +1,14 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "localhost"
+target_port = 9999
 
 # AF_INET: ipv4, SOCK_STREAM: TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send("hello~")
 
 response = client.recv(4096)
 

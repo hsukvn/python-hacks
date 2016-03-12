ddb6159f0a7c3c97b20b49da28d718c8f212a1b1
kevinhsu <kevinhsu@synology.com>
2016-03-12 21:33:04 +0800

    udp_client.py

---
+++ net/udp_client.py
@@ -0,0 +1,13 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+# AF_INET: ipv4, DGRAM: UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.sendto("Hello", (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print data, addr
